136d299b8268e7f2092db8617583a26c271af20b
user-root <usersolo1@protonmail.com>
2026-06-04 23:14:50 +0000

    [+] Add ui file statusline: Native implementation of status-line in nvim

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -11,7 +11,7 @@ local function git_branch()
 		last_check = now
 	end
 	if cached_branch ~= "" then
-		return "  " .. cached_branch .. " "
+		return " " .. cached_branch
 	end
 	return ""
 end
@@ -19,18 +19,16 @@ end
 local function file_type()
 	local ft = vim.bo.filetype
 	if ft == "" then
-		return "  "
+		return ""
 	end
-	return "  " .. ft
+	return " " .. ft
 end
 
 local function file_size()
 	local size = vim.fn.getfsize(vim.fn.expand("%"))
 	if size < 0 then
 		return ""
-	end
-
-	if size < 1024 then
+	elseif size < 1024 then
 		return size .. "B"
 	elseif size < 1024 * 1024 then
 		return string.format("%.1fK", size / 1024)
@@ -47,18 +45,40 @@ local function mode_icon()
 		v = "VISUAL",
 		V = "V-LINE",
 		c = "COMMAND",
+		R = "REPLACE",
 	}
 	return map[mode] or mode
 end
 
+-- Colores por modo
+local function mode_color()
+	local mode = vim.fn.mode()
+	local colors = {
+		n = "%#StatusLineNormal#",
+		i = "%#StatusLineInsert#",
+		v = "%#StatusLineVisual#",
+		V = "%#StatusLineVisual#",
+		c = "%#StatusLineCommand#",
+		R = "%#StatusLineReplace#",
+	}
+	return colors[mode] or "%#StatusLineNormal#"
+end
+
 function M.setup()
+	-- Define highlight groups para colores
+	vim.cmd("highlight StatusLineNormal guifg=#FFFFFF guibg=#2E3440")
+	vim.cmd("highlight StatusLineInsert guifg=#000000 guibg=#A3BE8C")
+	vim.cmd("highlight StatusLineVisual guifg=#000000 guibg=#EBCB8B")
+	vim.cmd("highlight StatusLineCommand guifg=#000000 guibg=#B48EAD")
+	vim.cmd("highlight StatusLineReplace guifg=#000000 guibg=#BF616A")
+
 	vim.opt.statusline = table.concat({
-		" ",
-		"%{v:lua.require'config.statusline'.mode()} ",
-		"%f %h%m%r",
+		"%{v:lua.require'config.statusline'.mode_color()}",
+		" %{v:lua.require'config.statusline'.mode()}",
+		" %f %h%m%r",
 		"%{v:lua.require'config.statusline'.git()}",
-		" %= ",
-		"%l:%c %p%%",
+		" %=%{v:lua.require'config.statusline'.filetype()}",
+		" %{v:lua.require'config.statusline'.size()}",
 	})
 end
 
@@ -73,6 +93,9 @@ function M.size()
 end
 function M.mode()
 	return mode_icon()
+end
+function M.mode_color()
+	return mode_color()
 end
 
 return M
