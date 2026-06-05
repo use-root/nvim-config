feaa08c79fd1b25b3399e32343fcc8739326da82
user-root <usersolo1@protonmail.com>
2026-06-05 16:14:23 +0000

    [+] Delete config file highlights

--- lua/config/highlights.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e1e2e", fg = "#cdd6f4" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#1e1e2e", fg = "#f5c2e7" })
-
--- Diagnostics
-vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#f38ba8" })
-vim.api.nvim_set_hl(0, "DiagnosticWarn", { fg = "#fab387" })
-vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "#89b4fa" })
-vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "#94e2d5" })
