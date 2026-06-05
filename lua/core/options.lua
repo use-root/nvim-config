e655f8f0ef60ab4b62d5f1a6cff08826674dd18a
user-root <usersolo1@protonmail.com>
2026-06-05 18:02:36 +0000

    [+] Update core file options: Change the opt (wrap,winblend and pumblend)

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,9 +2,9 @@
 vim.opt.number = true -- Line numbers
 vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.cursorline = false -- Highlight current line
-vim.opt.wrap = false -- Don't wrap lines
-vim.opt.scrolloff = 10 -- Keep 10 lines above/below cursor
-vim.opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+vim.opt.wrap = true -- Don't wrap lines
+vim.opt.scrolloff = 12 -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 10 -- Keep 8 columns left/right of cursor
 
 -- Indentation
 vim.opt.tabstop = 2 -- Tab width
@@ -23,18 +23,18 @@ vim.opt.incsearch = true -- Show matches as you type
 vim.opt.termguicolors = true -- Enable 24-bit colors
 vim.opt.signcolumn = "yes" -- Always show sign column
 vim.opt.showmatch = true -- Highlight matching brackets
-vim.opt.matchtime = 2 -- How long to show matching bracket
-vim.opt.cmdheight = 1 -- Command line height
+vim.opt.matchtime = 4 -- How long to show matching bracket
+vim.opt.cmdheight = 2 -- Command line height
 vim.opt.completeopt = "menuone,noinsert,noselect" -- Completion options
 vim.opt.showmode = false -- Don't show mode in command line
-vim.opt.pumheight = 10 -- Popup menu height
-vim.opt.pumblend = 10 -- Popup menu transparency
-vim.opt.winblend = 0 -- Floating window transparency
+vim.opt.pumheight = 15 -- Popup menu height
+vim.opt.pumblend = 15 -- Popup menu transparency
+vim.opt.winblend = 20 -- Floating window transparency
 vim.opt.conceallevel = 0 -- Don't hide markup
 vim.opt.concealcursor = "" -- Don't hide cursor line markup
 vim.opt.lazyredraw = true -- Don't redraw during macros
-vim.opt.synmaxcol = 300 -- Syntax highlighting limit
-vim.opt.fillchars = { eob = " " } -- Hide ~ on empty lines
+vim.opt.synmaxcol = 500 -- Syntax highlighting limit
+vim.opt.fillchars = { eob = " " } -- Hide ~ on empty lines in your files
 
 -- File handling
 vim.opt.backup = false -- Don't create backup files
