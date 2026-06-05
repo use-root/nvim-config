09d582aebf784503be1b164cd47a1eb4a5d83cd2
user-root <usersolo1@protonmail.com>
2026-06-05 17:57:46 +0000

    [+] Update config file telescope: return a function with the configuration and custom the layout

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,51 +1,80 @@
 local actions = require("telescope.actions")
 local telescope = require("telescope")
+return function()
+	telescope.setup({
+		defaults = {
+			layout_strategy = "center",
+			path_display = { "smart" },
+			file_ignore_patterns = { "node_modules", ".git/" },
+			color_devicons = true,
+			sorting_strategy = "ascending",
+			borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+			layout_config = {
 
-telescope.setup({
-	defaults = {
-		prompt_prefix = "🔍 ",
-		selection_caret = "> ",
-		layout_strategy = "flex",
-		path_display = { "smart" },
-		file_ignore_patterns = { "node_modules", ".git/" },
-		color_devicons = true,
-		sorting_strategy = "ascending",
-		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-		layout_config = {
-			horizontal = { width = 0.9, preview_width = 0.6 },
-			vertical = { width = 0.9, height = 0.8 },
-			flex = { horizontal = { preview_width = 0.6 }, vertical = { preview_width = 0.5 } },
-		},
-		mappings = {
-			i = {
-				["<C-j>"] = actions.move_selection_next,
-				["<C-k>"] = actions.move_selection_previous,
-				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-				["<C-d>"] = actions.delete_buffer,
-				["<esc>"] = actions.close,
-			},
-			n = {
-				["q"] = actions.close,
-				["dd"] = actions.delete_buffer,
-				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-			},
-		},
-	},
-	pickers = {
-		find_files = { theme = "dropdown" }, -- sin sidebar, compacto
-		buffers = { theme = "dropdown" },
-		help_tags = { theme = "dropdown" },
-		live_grep = { layout_strategy = "vertical" },
-	},
-	extensions = {
-		fzf = {
-			fuzzy = true, -- búsqueda borrosa
-			override_generic_sorter = true,
-			override_file_sorter = true,
-			case_mode = "smart_case",
-		},
-	},
-})
+				center = {
+					height = 0.4,
+					width = 0.6,
+					preview_cutoff = 20,
+					prompt_position = "top",
+				},
 
--- cargar extensión fzf
-telescope.load_extension("fzf")
+				flex = { horizontal = { preview_width = 0.6 }, vertical = { preview_width = 0.8 } },
+			},
+			mappings = {
+				i = {
+					["<C-j>"] = actions.move_selection_next,
+					["<C-k>"] = actions.move_selection_previous,
+					["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+					["<C-d>"] = actions.delete_buffer,
+					["<C-h>"] = actions.select_horizontal,
+					["<C-e>"] = actions.select_vertical,
+					["<C-t>"] = actions.select_tab,
+					["<C-/>"] = actions.which_key, -- Show all available mappings
+					["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+					["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				},
+				n = {
+					["q"] = actions.close,
+					["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+					["dd"] = actions.delete_buffer,
+					["<C-h>"] = actions.select_horizontal,
+					["<C-e>"] = actions.select_vertical,
+					["<C-t>"] = actions.select_tab,
+				},
+			},
+		},
+		pickers = {
+			layout_strategy = "vertical",
+			layout_config = {
+				flex = {
+					preview_width = 0.6,
+					height = 0.9,
+					width = 0.6,
+				},
+				vertical = {
+					preview_width = 0.8,
+					height = 0.9,
+					width = 0.6,
+				},
+			},
+		},
+		buffers = {
+			mappings = {
+				n = {
+					["dd"] = actions.delete_buffer,
+				},
+			},
+		},
+		live_grep = {},
+		extensions = {
+			fzf = {
+				fuzzy = true,
+				override_generic_sorter = true,
+				override_file_sorter = true,
+				case_mode = "smart_case",
+			},
+		},
+	})
+
+	telescope.load_extension("fzf")
+end
